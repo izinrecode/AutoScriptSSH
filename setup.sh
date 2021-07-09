92c9c51878b97ebd82a6eee18457e1e0c651cf01
izinrecode <83387876+izinrecode@users.noreply.github.com>
2021-07-10 04:31:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -49,7 +49,7 @@ clear
 echo " "
 echo "Installation has been completed!!"
 echo " "
-echo "=================================-Autoscript Premium by JOMBLOSSH-===========================" | tee -a log-install.txt
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━Autoscript Premium by JOMBLOSSH━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
@@ -84,7 +84,7 @@ echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
 echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "------------------------------------------BY JSTFREENET------------------------------------------" | tee -a log-install.txt
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━BY JSTFREENET━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a log-install.txt
 echo ""
 echo " Reboot 10 Sec"
 sleep 10
