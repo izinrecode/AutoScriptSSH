d3ff751321d5d205add468db8ba78a517f9727a0
izinrecode <83387876+izinrecode@users.noreply.github.com>
2021-07-10 03:37:18 +0700

    Update member.sh

--- member.sh
+++ member.sh
@@ -16,4 +16,4 @@ JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)
 echo "-------------------------------"
 echo "Jumlah akun: $JUMLAH user"
 echo "-------------------------------"
-echo -e "by HAWENG"
+echo -e "by JOMBLOSSH"
