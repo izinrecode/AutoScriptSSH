5c716de52bb52adaa6a911fef7b8a9b63b00543c
izinrecode <83387876+izinrecode@users.noreply.github.com>
2021-07-10 04:05:47 +0700

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cleae
-echo Installing Websocket-SSH AUTOSCRIPT BY HABLESSH
+echo Installing Websocket-SSH AUTOSCRIPT BY JOMBLOSSH
 sleep 1
 echo Cek Hak Akses...
 sleep 0.5
 cd
 #Install system auto run
-wget -O /etc/systemd/system/ws-ssh.service https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ws-ssh.service && chmod +x /etc/systemd/system/ws-ssh.service
-wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/834e18746c6a3d822aef0a65cc9f3ec470bb6e52/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
+wget -O /etc/systemd/system/ws-ssh.service https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/ws-ssh.service && chmod +x /etc/systemd/system/ws-ssh.service
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
 #Install Websocket-SSH
 
-wget -O /usr/local/bin/ws-ssh https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ws-ssh && chmod +x /usr/local/bin/ws-ssh
-wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
+wget -O /usr/local/bin/ws-ssh https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/ws-ssh && chmod +x /usr/local/bin/ws-ssh
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #Enable & Start ws-ssh service
 systemctl enable ws-ssh.service
