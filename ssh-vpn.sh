e57cd7ed191307e5197b3ecdbeb8a2056184ff5d
izinrecode <83387876+izinrecode@users.noreply.github.com>
2021-07-26 00:29:30 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -95,7 +95,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/nginx.conf"
 mkdir -p /home/vps/public_html
-echo "<pre>Setup by Haweng</pre>" > /home/vps/public_html/index.html
+echo "<pre>Setup by JOMBLOSSH.TECH</pre>" > /home/vps/public_html/index.html
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/vps.conf"
 /etc/init.d/nginx restart
 
