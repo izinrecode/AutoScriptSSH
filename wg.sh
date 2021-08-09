a4d197e6e3074827203108b7685a58635c44295e
izinrecode <83387876+izinrecode@users.noreply.github.com>
2021-08-09 16:25:54 +0700

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -23,7 +23,7 @@ if [[ -e /etc/wireguard/params ]]; then
 	exit 1
 fi
 
-echo -e "${Info} Wireguard VPS AutoScript by Haweng"
+echo -e "${Info} Wireguard VPS AutoScript by JOMBLOSSH"
 # Detect public IPv4 address and pre-fill for the user
 
 # Detect public interface and pre-fill for the user
